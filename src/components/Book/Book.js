b0aa6551fe81b3b4b42102d18faaab036952a262
Tareq <tarequl30@gmail.com>
2021-03-27 17:41:48 +0600

    updated

--- src/components/Book/Book.js
+++ src/components/Book/Book.js
@@ -18,7 +18,16 @@ const Book = () => {
         };
 
         const handleBooking = () => {
-
+            const newBooking = {...loggedInUser, ...selectedDate}
+            fetch('/http://localhost:5000/addBooking', {
+                method: 'POST',
+                headers: {'Content-Type': 'application/json'},
+                body: JSON.stringify(newBooking)
+            })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data)
+            })
         }
     const {bedType} = useParams();
     return (
